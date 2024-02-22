c82a3871e74e95cb7c4ef332522f0dfe6e8f1d2f
jaychodvadiy <jaychodvadiya130@gmail.com>
2024-02-22 18:10:25 +0530

    compoents file creat and layout

---
+++ src/components/customercard/customecard.js
@@ -0,0 +1,73 @@
+import Image from 'next/image'
+import React from 'react'
+import Asset28 from '../../assets/images/Asset 2 8.png'
+import Asset from '../../assets/images/Asset 3.png'
+import Asset3 from '../../assets/images/Asset 5 3.png'
+function Customecard() {
+  return (
+    <div>
+        <section className="buy-rent-sell-mmain">
+        <div className="container">
+          <div className="row ">
+            <div className="col-lg-12">
+              <div className="baynoe">
+                <p>BUY . RENT . SELL</p>
+                <h2 className="help">
+                  We help people buy, rent and sell homes
+                </h2>
+                <p className="makereal">
+                  We're here to make real estate easy. Whether you're buying,
+                  renting, or selling, our dedicated team is ready to assist you
+                  every step of the way. With our expertise and network, we
+                  ensure a <br />
+                  seamless experience.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-lg-4">
+              <div className="servier">
+                <Image
+                  src={Asset28}
+                  alt="Asset28"
+                  className="imgfluid img-fluid"
+                />
+              </div>
+              <h3 className="mb-1 mb-md-3 text-center">Qualified Agents</h3>
+              <p className="pb-0 sellhomedescription">
+                When it comes to finding your drem property or selling your
+                home, trust in our team of highly qualified agents. Our experts
+                bring years of industry experience and deep knowledge of the
+                local real estate market to the table.
+              </p>
+            </div>
+            <div className="col-lg-4">
+              <div className="servier">
+                <Image className="imgfluid" src={Asset} alt="Asset" />
+              </div>
+              <h3 className="mb-1 mb-md-3 text-center">Excellent service</h3>
+              <p className="pb-0 sellhomedescription">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+                tellus, luctus nec ullamcorper mattis, pulvinar leo.
+              </p>
+            </div>
+            <div className="col-lg-4">
+              <div className="servier">
+                <Image className="imgfluid" src={Asset3} alt="Asset3" />
+              </div>
+              <h3 className="mb-1 mb-md-3 text-center">Customer Care</h3>
+              <p className="pb-0 sellhomedescription">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+                tellus, luctus nec ullamcorper mattis, pulvinar leo.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Customecard
---
+++ src/components/lookinghoom/lookinghome.js
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import highangle from '../../assets/images/high-angle-pie-chart-with-cities 1.png'
+
+function Lookinghome() {
+  return (
+    <div>
+      <section className="cta sellhome bg-transparent">
+        <div className="container">
+          <div className="property">
+            <div className="row">
+              <div className="col-sm-6 cta-info ctn">
+                <h3 className="">Looking to sell your home?</h3>
+                <h6 className="">
+                  Unlock a hassle-free selling experience and unleash the real
+                  potential of your cherished property.
+                </h6>
+                <Link href="/listproperty">
+                  <button className="d-none d-md-flex align-items-center justify-content-center thiyo-btn">
+                    LIST YOUR PROPERTY
+                  </button>
+                </Link>
+                <button className="d-flex align-items-center justify-content-center d-md-none thiyo-btn">
+                  CONTACT US
+                </button>
+              </div>
+              <div className="col-lg-6">
+                <Image
+                 src={highangle} alt="highangle" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Lookinghome;
