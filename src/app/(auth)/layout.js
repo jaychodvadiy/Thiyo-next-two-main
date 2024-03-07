feb6febffd529585ff90e2dd5825e9858359b0a5
jaychodvadiy <jaychodvadiya130@gmail.com>
2024-03-07 14:55:23 +0530

    remove style layout

--- src/app/(auth)/layout.js
+++ src/app/(auth)/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "../globals.css";
-// import "../../assets/style1.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
